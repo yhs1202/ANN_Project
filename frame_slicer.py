ea13ee20d43d270f0efc31fe8fd2d3d6ce58080f
hsyoon_mbp <dbsghtmd99@naver.com>
2023-05-22 22:40:23 +0900

    add main source,  if __name__ == '__main__':

--- frame_slicer.py
+++ frame_slicer.py
@@ -15,26 +15,26 @@ def save_frames(video_path, output_dir):
 
     frame_num = 0
     while success:
-        if frame_num % int(fps / 60) == 0:  # 60프레임으로 잘라서 저장
+        if frame_num % int(fps / 30) == 0:  # 60프레임으로 잘라서 저장
             frame_path = f"{output_dir}/frame_{frame_num}.jpg"
             cv2.imwrite(frame_path, frame)
 
         success, frame = cap.read()
         frame_num += 1
-
     cap.release()
-
     print(f"Frames saved to {output_dir}.")
 
-# 동영상 파일 경로 설정
-video_path = './NIA_SL_FS0001_CROWD18_F/NIA_SL_FS0001_CROWD18_F.mp4'
 
-# 이미지 프레임 저장할 디렉토리 경로 설정
-output_dir = './NIA_SL_FS0001_CROWD18_F/res'
+if __name__ == '__main__':
+    # 동영상 파일 경로 설정
+    video_path = './NIA_SL_FS0001_CROWD18_F/NIA_SL_FS0001_CROWD18_F.mp4'
 
-# 디렉토리 생성
-import os
-os.makedirs(output_dir, exist_ok=True)
+    # 이미지 프레임 저장할 디렉토리 경로 설정
+    output_dir = './NIA_SL_FS0001_CROWD18_F/res'
 
-# 동영상 프레임 저장
-save_frames(video_path, output_dir)
+    # 디렉토리 생성
+    import os
+    os.makedirs(output_dir, exist_ok=True)
+
+    # 동영상 프레임 저장
+    save_frames(video_path, output_dir)
