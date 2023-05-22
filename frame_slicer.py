ac80cbf949b7588daf7b2db35868d5843fbb428f
hsyoon_mbp <dbsghtmd99@naver.com>
2023-05-23 00:26:38 +0900

    Blur Detection complete

--- frame_slicer.py
+++ frame_slicer.py
@@ -1,6 +1,13 @@
 import cv2
+import shutil
+import os
 
 def save_frames(video_path, output_dir):
+    
+    if os.path.isdir(output_dir):
+        shutil.rmtree(output_dir)
+        os.makedirs(output_dir, exist_ok=True)
+
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print("Failed to open video file.")
@@ -15,7 +22,7 @@ def save_frames(video_path, output_dir):
 
     frame_num = 0
     while success:
-        if frame_num % int(fps / 30) == 0:  # 60프레임으로 잘라서 저장
+        if frame_num % int(fps / 24) == 0:  # 30프레임으로 잘라서 저장
             frame_path = f"{output_dir}/frame_{frame_num}.jpg"
             cv2.imwrite(frame_path, frame)
 
@@ -27,13 +34,12 @@ def save_frames(video_path, output_dir):
 
 if __name__ == '__main__':
     # 동영상 파일 경로 설정
-    video_path = './NIA_SL_FS0001_CROWD18_F/NIA_SL_FS0001_CROWD18_F.mp4'
+    video_path = '/Users/hsyoon/Downloads/mov/KakaoTalk_Video_2023-05-22-22-30-01.mp4'
 
     # 이미지 프레임 저장할 디렉토리 경로 설정
-    output_dir = './NIA_SL_FS0001_CROWD18_F/res'
+    output_dir = '/Users/hsyoon/Downloads/img/'
 
     # 디렉토리 생성
-    import os
     os.makedirs(output_dir, exist_ok=True)
 
     # 동영상 프레임 저장
